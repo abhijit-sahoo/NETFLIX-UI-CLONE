9f4ce4d80d003930de344b9d0b49541a7738a10d
abhijit-sahoo <abhijitsahoo.in@gmail.com>
2025-08-05 07:27:46 +0530

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,56 @@
+
+import { initializeApp } from "firebase/app";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import {toast} from 'react-toastify'
+
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+ 
+console
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signUp = async (name,email, password) =>{
+ try {
+   const res =  await createUserWithEmailAndPassword(auth,email,password);
+   const user = res.user;
+   await addDoc(collection(db,'user'),{
+    uid:user.uid,
+    name,
+    authProvider:"local",
+    email,
+   })
+
+ } catch (error) {
+    console.log(error);
+    toast.error(error.code)
+    
+ }
+}
+
+const login = async (email,password)=>{
+    try {
+        await signInWithEmailAndPassword(auth,email,password)
+        
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code)
+    }
+}
+
+const logout = () =>{
+    signOut(auth)
+}
+
+export {auth,db,login,signUp,logout};
